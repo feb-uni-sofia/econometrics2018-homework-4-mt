9c998b325da62c8b15692fa5f7a6083c0a143ef5
Mirelatsonova <37420597+Mirelatsonova@users.noreply.github.com>
2018-06-15 13:12:54 +0200

    Add files via upload

--- Problem_1.R
+++ Problem_1.R
@@ -3,25 +3,59 @@
 library(dplyr)
 
 ## Read the data
-houseWork <- read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/houseWork.csv')
+houseWork <-
+  read.csv('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/houseWork.csv')
 str(houseWork)
 
 ## a)
+table(houseWork$sex)
 
 ## b)
+muf <- mean(houseWork [houseWork$sex == 'f', "hours"])
+mum <- mean(houseWork [houseWork$sex == 'm', "hours"])
 
 ## c)
+houseworkbysex <-
+  within(houseWork, {
+    female <- ifelse (sex == 'f', 'TRUE', 'FALSE')
+    male <- ifelse (sex == 'm', 'TRUE', 'FALSE')
+  })
 
 ## d)
+fit <- lm(hours ~ female , data = houseworkbysex)
 
 ## e)
+summary(fit)
+18.35829 - 32.81379
+# The estimate of beta0 is 32.8138. It shows the population average house-work hours for men.
+# The estimate of beta1 = -14.4555  is the difference between the population average house-work hours for women and men.
 
 ## f)
+# H0: muf <= mum means muf - mum <= 0
+# but we already know that beta1 =  muf - mum
+# so H0: beta1 <= 0 vs. H1: beta1 > 0
 
 ## g)
+beta1 <- -14.4555
+n <- nrow(houseworkbysex)
+
+testStatistic <- sqrt(n) * (beta1 / sd(houseworkbysex$hours))
+criticalUpper <- qt(0.95, df = n - 1)
+testStatistic > criticalUpper
+pvalue <- (1 - pt(testStatistic, df = n - 1))
+pvalue
 
 ## h)
+# We cannot reject the null nypothesis
+## at a 95% significance level (5% error probability)
+## because the p-value is larger than 0.05. The p-value equals 1.
 
 ## i)
+sd(houseworkbysex$hours)
+mean(houseworkbysex$hours)
+#the distributional assumptions of the test
+#are that the observations are normally distributed.
 
 ## j)
+fitAll <- lm(hours ~ female  + male , data = houseworkbysex)
+summary(fitAll)
--- Problem_2.R
+++ Problem_2.R
@@ -1,13 +1,65 @@
 ## Homework 4, Problem 2
-crime <- read.delim('https://s3.eu-central-1.amazonaws.com/econometrics2018/data/crime.csv', stringsAsFactors = FALSE)
+crime <-
+  read.delim(
+    'https://s3.eu-central-1.amazonaws.com/econometrics2018/data/crime.csv',
+    stringsAsFactors = FALSE
+  )
 str(crime)
+#install.packages(c('ggplot2', 'dplyr'))
+
+library(dplyr)
+library(ggplot2)
 
 ## a)
+fit <- lm(C ~ HS , data = crime)
+summary(fit)
+##Beta1 is 1.4860, which is >0.
+#This means that when HS increases C increases as well.
+#Higher percent of people completing high school is connected with higher crime rate.
 
 ## b)
+ggplot(data = crime, aes(x = HS, y = C)) +
+  geom_point() +
+  geom_abline(slope = 1.4860, intercept = -50.8569)
+
+ggplot(data = crime, aes(x = U, y = C)) +
+  geom_point()
+
+ggplot(data = crime, aes(x = I, y = C)) +
+  geom_point()
 
 ## c)
+fit2 <- lm(C ~ HS + U, data = crime)
+summary(fit2)
+#Beta1 is -0.5834, which is negative.
+#When the percentage of people completing high school increases
+#the crime rate decreases.
+
+#Beta 2 is  0.6825, which is positive.
+#This means that when the percentage of people living in urban areas increases
+#the crime rate also increases.
+
 
 ## d)
+fitHS_U <- lm(HS ~ U , data = crime)
+summary(fitHS_U)
+#we can notice that there is a positive connection between urbanization and people finishing high school.
+#The percantage of people with HS degree is higher in urban areas.
 
 ## e)
+fit3 <- lm(C ~ HS + U + I, data = crime)
+summary(fit3)
+
+var.test(
+  lm(C ~ HS , data = crime),
+  lm(C ~ I , data = crime) ,
+  ratio = 1,
+  alternative = c("two.sided"),
+  conf.level = 0.95
+)
+
+#P-value is greater than 0.05
+#so we cannot reject the null hypothesis at a 5% significance level.
+#H0 means:
+#1)there is no connection between people finishing high school and the crime rate
+#2)there is no connection between the median income in the county and the crime rate
